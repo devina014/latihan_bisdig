5a29e4179573ab2f5674a7eafcb501c1f235f77e
devina014 <devinaputriirawan0@gmail.com>
2025-09-10 15:32:18 +0700

    latihan_bisdig

---
+++ belajar.js
@@ -0,0 +1,31 @@
+// // Contoh alert
+//     alert("Hi there");
+
+//     // Contoh confirm
+//     var isOkay = confirm("I'm gonna do something, okay?");
+//     if (isOkay) {
+//         alert("You clicked OK");
+//     } else {
+//         alert("You clicked Cancel");
+//     }
+
+//     // Contoh prompt
+//     var userInput = prompt("What should I do?");
+//     if (userInput !== null) {
+//         alert("You typed: " + userInput);
+//     } else {
+//         alert("You canceled the prompt");
+//     }
+
+//     // alert("thanksdekk");
+
+//     // Number = 1 +3;
+//     // console.log(Number);
+
+var myInteger = 12 ;
+console.log(myInteger);
+
+myArray = [ "zero" , "one" , "two" ];
+window.alert ( myArray[ 0 ]);
+
+
